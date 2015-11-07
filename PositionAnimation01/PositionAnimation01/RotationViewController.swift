8b90f15683106726e588672e2dbe220b428364e7
chenyufeng <503434355@qq.com>
2015-11-07 15:58:17 +0800

    14

--- PositionAnimation01/PositionAnimation01/RotationViewController.swift
+++ PositionAnimation01/PositionAnimation01/RotationViewController.swift
@@ -36,11 +36,6 @@ class RotationViewController: UIViewController {
 
         // Do any additional setup after loading the view.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
     
     func spin(){
     
